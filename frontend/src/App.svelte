acad20747b499789f2afebbecb9bcbe8afb8b73f
Ofir Herzas <ofir.herzas@sparkbeyond.com>
2020-02-17 11:12:29 +0200

    add initial frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Search from './components/Search.svelte';
+	export let name;
+
+	function onSearchChange(value) {
+		console.log(value);
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Search on:change={onSearchChange} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
