d5ac7bef3b9c104a438314b14d2d4aa110ba8e09
Andrey S <novus42@gmail.com>
2020-02-17 13:34:15 +0200

    adding google trends search

--- backend/controllers/search.go
+++ backend/controllers/search.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"github.com/groovili/gogtrends"
 	"net/http"
 	"regexp"
 	"search-package/client"
@@ -15,6 +18,10 @@ func setResponseHeaders(w *http.ResponseWriter) {
 	(*w).Header().Set("Content-Type", "application/json")
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+}
+
+type trendsController struct {
+	query *regexp.Regexp
 }
 
 func (sc searchController) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -31,6 +38,7 @@ func (sc searchController) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	fmt.Println("Searching github for " + strMatches[1])
 	repoResp, err := client.SearchRepos(strMatches[1])
 	if err != nil {
 		panic(err)
@@ -43,6 +51,46 @@ func (sc searchController) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Write(j)
 }
 
+// trends
+func (tc trendsController) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	path := req.URL.Path
+	strMatches := tc.query.FindStringSubmatch(path)
+
+	if len(strMatches) < 2 {
+		w.Write([]byte("No trends query provided"))
+		return
+	}
+
+	fmt.Println("Searching trends for " + strMatches[1])
+
+	ctx := context.Background()
+	explore, err := gogtrends.Explore(ctx,
+		&gogtrends.ExploreRequest{
+			ComparisonItems: []*gogtrends.ComparisonItem{
+				{
+					Keyword: strMatches[1],
+					Geo:     "US",
+					Time:    "today+12-m",
+				},
+			},
+			Category: 31, // Programming category
+			Property: "",
+		}, "EN")
+
+	overTime, err := gogtrends.InterestOverTime(ctx, explore[0], "EN")
+	if err != nil {
+		panic(err)
+	}
+
+	j, err := json.Marshal(overTime)
+	if err != nil {
+		panic("Could not serialize")
+	}
+	w.Write(j)
+}
+
 // RegisterSearchController registers the search controller as an http handler
 func RegisterSearchController() {
 	sc := searchController{
@@ -51,4 +99,12 @@ func RegisterSearchController() {
 
 	http.Handle("/query", sc)
 	http.Handle("/query/", sc)
+
+	// Google Trends
+	tc := trendsController{
+		query: regexp.MustCompile(`^/trends/(.+)/?`),
+	}
+	http.Handle("/trends", tc)
+	http.Handle("/trends/", tc)
+
 }
