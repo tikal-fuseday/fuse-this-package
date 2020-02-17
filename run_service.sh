8fdd84cc1e6601eaa2bd5c554e39ccd1a5dbd0b2
Andrey S <novus42@gmail.com>
2020-02-17 14:28:34 +0200

    adding dockerfile for frontend

--- run_service.sh
+++ run_service.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 cd backend
-docker build -t fuse_go_image .
+docker build -t fuse-go-image .
 cd ..
 echo "Service running at http://localhost:3000"
-docker run -it --rm --name tmp -p 3000:3000 fuse_go_image:latest
+docker run -it --rm -d --name tmp -p 3000:3000 fuse-go-image:latest
+
+cd frontend
+docker build -t fuse-svelte-image .
+cd ..
+echo "Frontend running at http://localhost:8080"
+docker run -it --rm -d --name tmp1 -p 80:80 fuse-svelte-image:latest
 
 
