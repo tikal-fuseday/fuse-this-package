e18a7ae4fb379ab54ca1788f6c20f867325a2034
Robert Ferentz <RobiFerentz@gmail.com>
2020-02-17 13:31:02 +0200

    added authorization header to github api request

--- backend/client/client.go
+++ backend/client/client.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	authToken = "blahblah"
+	authToken = "145d0586fffb2790d22f45bb76e8f629131094fc"
 	apiURL    = "https://api.github.com"
 )
 
@@ -17,12 +17,13 @@ var c *http.Client = &http.Client{}
 
 // SearchRepos retrieves a github repo search response
 func SearchRepos(search string) (models.GithubRepoSearchResponse, error) {
-	fmt.Println("Fetching results for :", search)
+	fmt.Println("SearchRepos fetching results for :", search)
 	reposURL := apiURL + "/search/repositories?sort=stars&order=desc&q=" + search + "+language:javascript"
 	repoResp := models.GithubRepoSearchResponse{}
 
 	req, _ := http.NewRequest("GET", reposURL, nil)
 	req.Header.Set("Accept", "application/vnd.github.v3+json")
+	req.Header.Set("Authorization", "token " + authToken)
 	resp, err := c.Do(req)
 	if err != nil {
 		return repoResp, err
