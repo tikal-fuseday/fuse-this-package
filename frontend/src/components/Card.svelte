d657e035a9b4f4b5008067dd18619c6cd10c26fd
herzaso@gmail.com <ofir.herzas@sparkbeyond.com>
2020-02-17 13:41:13 +0200

    cont frontend

---
+++ frontend/src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+export let item;
+</script>
+
+<style>
+  .card-container {
+    border: 1px solid grey;
+    border-radius: 10px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    color: darkblue;
+  }
+</style>
+
+<section class="card-container">
+  <header class="card-header" >{item.name}</header>
+  <main >{item.description}</main>
+  <footer class="card-footer" >
+</section>
---
+++ frontend/src/components/ResultList.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Card from "./Card.svelte";
+  export let items = [];
+</script>
+
+<style>
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+  }
+  li {
+    margin: 10px;
+  }
+</style>
+
+<ul>
+  {#each items as item}
+    <li>
+      <Card {item} />
+    </li>
+  {/each}
+</ul>
