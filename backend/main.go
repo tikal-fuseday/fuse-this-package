349f0a6518904487e0df45122dc52c21d4c88445
Robert Ferentz <RobiFerentz@gmail.com>
2020-02-22 13:21:38 +0200

    added max procs and removed need to 'allow' clicks on osx

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 	"search-package/controllers"
 	"strconv"
 )
@@ -10,7 +11,8 @@ import (
 var port = 3000
 
 func main() {
+	runtime.GOMAXPROCS(4)
 	controllers.RegisterSearchController()
 	fmt.Printf("%s %d \n", "Server running on port", port)
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	http.ListenAndServe("localhost:"+strconv.Itoa(port), nil)
 }
